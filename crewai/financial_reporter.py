4517966ac402c4c365ed5c2131319f669dcdfe79
Kevin Duck <kevingduck@github.com>
2024-01-06 18:11:26 -0500

    Update financial_reporter.py

--- crewai/financial_reporter.py
+++ crewai/financial_reporter.py
@@ -3,12 +3,14 @@ This script sets up an automated workflow using CrewAI and LangChain libraries t
 
 1. Initializes an Ollama open source language model for processing natural language tasks.
 2. Defines a DuckDuckGo search tool for information retrieval.
-3. Creates three specialized agents - a Researcher, a Writer, and a Presenter - each with distinct roles and goals related to financial analysis and content creation.
-4. Assigns specific tasks to each agent: the Researcher analyzes financial statements, the Writer creates content based on the analysis, and the Presenter designs a PowerPoint presentation.
+3. Creates two specialized agents - a Researcher and a Presentation Writer - each with distinct roles and goals related to financial analysis and content creation.
+4. Assigns specific tasks to each agent: the Researcher analyzes financial statements, then the Writer creates content based on the analysis that can be used to generate a presentation.
 5. Organizes these agents and their tasks into a Crew, executing the tasks in a sequential process.
 6. Finally, the script runs the workflow and prints the results, which include research findings, written content, and presentation details.
 
-This automated system demonstrates the integration of natural language processing, task delegation, and workflow management for efficient business intelligence and content creation.
+You need ollama installed (see ollama.ai). This example uses the model 'mistral:instruct', which you can download in a terminal with `ollama run mistral:instruct`.
+
+You also need crewai and duckduckgo-search installed with `pip install -U crewai duckduckgo-search`
 """
 
 
@@ -16,7 +18,7 @@ import os
 from crewai import Agent, Task, Crew, Process
 from langchain.llms import Ollama
 
-ollama_mistral = Ollama(model="mistral")
+ollama_mistral = Ollama(model="mistral:instruct")
 
 # Define your tools, custom or not.
 # Install duckduckgo-search for this example:
@@ -70,34 +72,21 @@ presenter = Agent(
 # Create tasks for your agents
 task1 = Task(
   description="""Research the specified companies' most recent financial statements and
-  analyze their financial trends. Compile your findings in a detailed report. 
-  Make sure to include links to the financial statements you used, when possible.""",
+  analyze their financials. Compile your findings in a detailed report. """,
   agent=researcher
 )
 
 task2 = Task(
-  description="""Using the insights from the researcher's report, craft a
-  compelling summary outline with bullet points that highlights the latest 
-  financial trends that can be handed to the presentation specialist to create 
-  a PowerPoint presentation.""",
-  agent=writer
-)
-
-task3 = Task(
-  description="""Using the insights from the researcher's report and the
-  writer's article, create a PowerPoint presentation that highlights the latest
-  financial trends. Make sure to include links to the financial statements you
-  used. Also make sure to include a description of the images used for each slide. 
-  You MUST return the text of the script for the presenter to read, a 
-  description of the content on the slides, and a description of the images used 
-  in each slide.""",
+  description="""Using the researcher's report, create the script for a presentation. 
+  You MUST return the text of the script for the presenter to read, a description of 
+  the content on the slides, and a description of the images used in each slide.""",
   agent=presenter
 )
 
 # Instantiate your crew with a sequential process
 crew = Crew(
-  agents=[researcher, writer, presenter],
-  tasks=[task1, task2, task3],
+  agents=[researcher, presenter],
+  tasks=[task1, task2],
   verbose=2, # Crew verbose more will let you know what tasks are being worked on, you can set it to 1 or 2 to different logging levels
   process=Process.sequential # Sequential process will have tasks executed one after the other and the outcome of the previous one is passed as extra content into this next.
 )
